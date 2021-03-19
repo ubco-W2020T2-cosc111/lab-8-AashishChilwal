0286960cc669d4f7721af0e89a555590d8d5ac5d
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-19 13:41:00 -0600

    Done Q3

--- Q3.java
+++ Q3.java
@@ -1,1 +1,20 @@
+public class Q3
+{
+	public static void main(String[] args) {
+        int line = 10;	 
+		int counter = 0;
+
+		
+		for (int i = 100; i <= 200; i++) {
+			
+			if (i % 5 == 0 ^ i % 6 == 0) {
+				counter++;	
+				
+				if (counter % line == 0) 
+					System.out.println(i);
+				else
+					System.out.print(i + " ");		
+			}
+		}	}
+}
 
