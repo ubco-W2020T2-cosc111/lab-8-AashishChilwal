911e421975087fc7aebb63796eed08cf82642c2b
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-19 15:30:29 -0600

    Finished Q1

--- Q1.java
+++ Q1.java
@@ -4,29 +4,45 @@ public class Q1
  {
 	public static void main(String[] args) {
 	    double average = 0,sum = 0;
-	    int numlength = 0,positive = 0,negative = 0,even = 0,odd=0,i=1;
-	    double numbers [] = new double [i];
+	    int numlength = 0,positive = 0,negative = 0,even = 0,odd=0,i=100;
+	    double numbers;
 	    Scanner input = new Scanner(System.in);
-	    do
-		{
-		    System.out.print("Enter the first integer (0 to terminate): ");
-		    numbers[i] = input.nextInt();
-		    if (numbers [i] == 0)
-		        break;
-		     numlength++;
-		     sum = sum+numbers[i];
-		} while(true);
-		average = sum/numlength ; 
-		for (i = 0;i<numlength;i++)
-		{
-		  if (numbers[i] >= 0){
+	   System.out.print("Enter the first integer (0 to terminate): ");
+	   numbers = input.nextInt();
+	   numlength++;
+		     sum = sum+numbers;
+		     	average = sum/numlength ; 
+		  if (numbers >= 0){
 		    positive++;
 		  }
 		  else{
 		    negative++;
-		      
+		      }
+		  if (numbers % 2 == 0 )
+		  {
+		      even ++;
 		  }
-		  if (numbers[i] % 2 == 0 )
+		  else
+		  {
+		      odd ++;
+		  }
+	   if (numbers!=0){
+	   while(numbers !=0)
+		{
+		    System.out.print("Enter the first integer (0 to terminate): ");
+		    numbers = input.nextInt();
+		    if (numbers == 0)
+		        break;
+		     numlength++;
+		     sum = sum+numbers;
+		     	average = sum/numlength ; 
+		  if (numbers >= 0){
+		    positive++;
+		  }
+		  else{
+		    negative++;
+		      }
+		  if (numbers % 2 == 0 )
 		  {
 		      even ++;
 		  }
@@ -36,11 +52,15 @@ public class Q1
 		  }
 		        
 		}
+		
 		System.out.println("The number of positives is "+ positive);
 		System.out.println("The number of negatives is " + negative);
 		System.out.println("The number of evens is " + even);
 		System.out.println("The number of odds is " + odd);
-		System.out.println("The total is " + numlength);
+		System.out.println("The total is " + sum);
 		System.out.println("Average: " + average);
+	   }
+	   else
+	        System.out.println("No numbers are inputted except 0");
 	 }
  }
