a91a080c9a5edaea01b3e75061b0eb1a87678843
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-19 14:03:26 -0600

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+public class Q4
+{
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double score;
+        System.out.print("Enter the number of students: ");
+        int number = input.nextInt();
+        String [] name = new String [number] ;
+	    double [] score = new double [number];
+        for (int i = 0; i<number;i++)
+        {
+            System.out.println("Enter a student name: "); 
+             name[i] = input.nextLine();
+            System.out.println("\nEnter a student score");
+             score[i] = input.nextDouble();
+        }
+        for (i=1;i<number;i++)
+        {
+            if (score[i]>score[i-1])
+                highest = score[i];
+        }
+		}	
+}
