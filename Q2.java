3d5e70202173dcf53882fe069c14a157c2ca49d5
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-19 14:22:08 -0600

    Done Q2

--- Q2.java
+++ Q2.java
@@ -1,1 +1,17 @@
-
+public class Q2
+{
+	public static void main(String[] args) {
+	int sum;
+	System.out.print("The four perfect numbers are:");
+	for (int i=1;i<10000;i++){
+	    sum = 0;
+	   for (int k = 1; k < i; k++) {
+	       if (i % k == 0)
+					sum += k;
+	   }
+	   if (i == sum)
+	   System.out.print(" " + i);
+	}
+	
+	}
+}
